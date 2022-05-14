b1f6730996ee58d8a97fd3756d9f138fb87c5f17
Christian Müller <christian.mueller@hfu.ch>
2022-05-14 15:21:07 +0200

    Warum funktioniert Binding mit DataContext nicht???

---
+++ APE-Projekt/WPF_Binding_Textboxen/WPF_Binding_Textboxen/PersonViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Binding_Textboxen
+{
+    internal class PersonViewModel
+    {
+        public Person Person { get; set; }
+
+        public PersonViewModel()
+        { 
+            Person = new Person() { Name = "Grob", Vorname = "Hans", Alter = 50 };
+        }
+    }
+}
